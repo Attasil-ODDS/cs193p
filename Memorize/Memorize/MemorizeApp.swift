74f9ab0488820ca99d8f3087d17966f9f5290756
ProjectCoder <acgodsleeper@gmail.com>
2023-12-13 23:39:10 +0700

    Initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Attasil Chitwirote on 13/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
